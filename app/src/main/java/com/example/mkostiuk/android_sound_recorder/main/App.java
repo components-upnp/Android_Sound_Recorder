09b38e2fa3c94766a5f1eba27fdf85fe9dd70cff
MKostiuk <Mat01061995>
2017-06-21 17:16:41 +0200

    Implémentation générateur XML Implémentation interface graphique

--- app/src/main/java/com/example/mkostiuk/android_sound_recorder/main/App.java
+++ app/src/main/java/com/example/mkostiuk/android_sound_recorder/main/App.java
@@ -1,15 +1,92 @@
 package com.example.mkostiuk.android_sound_recorder.main;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mkostiuk.android_sound_recorder.R;
+import com.example.mkostiuk.android_sound_recorder.upnp.ServiceUpnp;
+
+import org.fourthline.cling.android.AndroidUpnpServiceImpl;
+
+import java.io.File;
 
 public class App extends AppCompatActivity {
+
+    private ServiceUpnp service;
+    private Button buttonRecord, buttonStopRecord;
+    private MediaRecorder recorder;
+
+    public void init() {
+        buttonRecord = (Button) findViewById(R.id.buttonRecord);
+        buttonStopRecord = (Button) findViewById(R.id.buttonStopRecord);
+        activate(buttonRecord);
+        deactivate(buttonStopRecord);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+
+        init();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        File dir;
+        System.err.println(Build.BRAND);
+        if (Build.BRAND.toString().equals("htc_europe"))
+            dir = new File("/mnt/emmc/SoundRecorder/");
+        else
+            dir = new File(Environment.getExternalStorageDirectory().getPath() + "/SoundRecorder/");
+
+        while (!dir.exists()) {
+            dir.mkdir();
+            dir.setReadable(true);
+            dir.setExecutable(true);
+            dir.setWritable(true);
+        }
+
+
+
+        service = new ServiceUpnp();
+
+
+
+
+        getApplicationContext().bindService(new Intent(this, AndroidUpnpServiceImpl.class),
+                service.getService(),
+                Context.BIND_AUTO_CREATE);
+
+
+    }
+
+    public void record(View view) {
+        activate(buttonStopRecord);
+        deactivate(buttonRecord);
+    }
+
+    public void stopRecord(View view) {
+        activate(buttonRecord);
+        deactivate(buttonStopRecord);
+    }
+
+    public void activate(Button ... buttons) {
+        for (Button b : buttons)
+            b.setClickable(true);
+    }
+
+    public void deactivate(Button ... buttons) {
+        for (Button b : buttons)
+            b.setClickable(false);
     }
 }
--- app/src/main/java/com/example/mkostiuk/android_sound_recorder/xml/GenerateurXml.java
+++ app/src/main/java/com/example/mkostiuk/android_sound_recorder/xml/GenerateurXml.java
@@ -1,8 +1,52 @@
 package com.example.mkostiuk.android_sound_recorder.xml;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 /**
  * Created by mkostiuk on 21/06/2017.
  */
 
 public class GenerateurXml {
+    public String getDocXml(String udn, String filePath) throws ParserConfigurationException, TransformerException {
+
+        String namespace = "";
+        Document doc;
+        DocumentBuilderFactory db = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+
+        builder = db.newDocumentBuilder();
+        doc = builder.newDocument();
+        Element root = doc.createElementNS(namespace, "SoundRecorder");
+        doc.appendChild(root);
+
+        Element nbQuestion = doc.createElementNS(namespace, "UDN");
+        root.appendChild(nbQuestion);
+        nbQuestion.appendChild(doc.createTextNode(udn));
+
+        Element command = doc.createElementNS(namespace, "CHEMIN");
+        root.appendChild(command);
+        command.appendChild(doc.createTextNode(filePath));
+
+        DOMSource source = new DOMSource(doc);
+        StringWriter writer = new StringWriter();
+        StreamResult result = new StreamResult(writer);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.transform(source, result);
+
+        return writer.toString();
+    }
 }
