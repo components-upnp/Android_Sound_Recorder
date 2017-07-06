0868af5aa0161da1c30e42ce8a634b41a319fb83
MKostiuk <Mat01061995>
2017-07-06 14:03:18 +0200

    Ajout popup demande autorisation accès stockage

--- app/src/main/java/com/example/mkostiuk/android_sound_recorder/main/App.java
+++ app/src/main/java/com/example/mkostiuk/android_sound_recorder/main/App.java
@@ -1,11 +1,15 @@
 package com.example.mkostiuk.android_sound_recorder.main;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Environment;
 import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -25,7 +29,11 @@ import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
+import xdroid.toaster.Toaster;
+
 public class App extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_STORAGE = 1;
 
     private ServiceUpnp service;
     private Button buttonRecord, buttonStopRecord;
@@ -35,20 +43,6 @@ public class App extends AppCompatActivity {
     private String pathFile;
 
     public void init() {
-        buttonRecord = (Button) findViewById(R.id.buttonRecord);
-        buttonStopRecord = (Button) findViewById(R.id.buttonStopRecord);
-        editText = (EditText) findViewById(R.id.editText);
-        recorder = new MediaRecorder();
-        activate(buttonRecord);
-        deactivate(buttonStopRecord);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app);
-
-        init();
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
@@ -72,17 +66,50 @@ public class App extends AppCompatActivity {
             dir.setWritable(true);
         }
 
-
-
         service = new ServiceUpnp();
-
-
 
 
         getApplicationContext().bindService(new Intent(this, AndroidUpnpServiceImpl.class),
                 service.getService(),
                 Context.BIND_AUTO_CREATE);
 
+        buttonRecord = (Button) findViewById(R.id.buttonRecord);
+        buttonStopRecord = (Button) findViewById(R.id.buttonStopRecord);
+        editText = (EditText) findViewById(R.id.editText);
+        recorder = new MediaRecorder();
+        activate(buttonRecord);
+        deactivate(buttonStopRecord);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app);
+
+        //Vérification que l'autorisation d'accès au système de stockage est accrodée
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                //Cela signifie que la permission à déjà était
+                //demandé et l'utilisateur l'a refusé
+                //Vous pouvez aussi expliquer à l'utilisateur pourquoi
+                //cette permission est nécessaire et la redemander
+                Toaster.toast("Vous avez refusé l'accés au Stockage, fermeture");
+                finish();
+            } else {
+                //Sinon demander la permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_STORAGE);
+            }
+        }
+        else {
+            //Permission déjà accrodée
+            init();
+        }
 
     }
 
@@ -119,5 +146,24 @@ public class App extends AppCompatActivity {
     public void deactivate(Button ... buttons) {
         for (Button b : buttons)
             b.setClickable(false);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // La permission est garantie on initialise les services et boutons
+                    init();
+                } else {
+                    Toaster.toast("Permission refusée, fermeture");
+                    finish();
+                }
+                return;
+            }
+        }
     }
 }
